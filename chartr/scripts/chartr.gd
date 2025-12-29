a5776cbf30aa095b2e7cff51e440dd5f32f00969
carth501 <carth501@gmail.com>
2025-12-29 11:54:34 -0500

    Reorganize logic for resizing and simplicity

--- chartr/scripts/chartr.gd
+++ chartr/scripts/chartr.gd
@@ -15,9 +15,12 @@ func display(x_values: Array, y_values: Array) -> void:
 	if x_values.size() != y_values.size():
 		push_warning("data size mismatch: x_values size %d, y_values size %d" % [x_values.size(), y_values.size()]);
 		return ;
-	var raw_points: PackedVector2Array = generate_point_array(x_values, y_values);
+	if x_values.size() < 2:
+		push_warning("Not enough points to plot.");
+		return ;
+	points = generate_point_array(x_values, y_values);
 
-	queue_plot(raw_points);
+	draw_queued = true;
 
 ## calculates the relative positions the points will be placed at, from 0 to 1.
 func generate_point_array(x_values: Array, y_values: Array) -> PackedVector2Array:
@@ -30,28 +33,25 @@ func generate_point_array(x_values: Array, y_values: Array) -> PackedVector2Arra
 				(x_values[i] - x_max_and_min["min"]) / x_max_and_min["max"],
 				(y_values[i] - y_max_and_min["min"]) / y_max_and_min["max"]
 				));
-	print("Raw points generated: %s" % raw_points);
 	return raw_points;
 
-func queue_plot(new_points: PackedVector2Array) -> void:
-	if new_points.size() < 2:
-		push_warning("Not enough points to plot.");
-		return ;
-	for point in new_points:
-		points.append(
+func _draw() -> void:
+	var scaled_points: PackedVector2Array = [];
+	for point in points:
+		scaled_points.append(
 			Vector2(
 				point.x * size.x,
 				size.y - (point.y * size.y)
 			)
 		);
-	print("Points queued for drawing: %s" % points);
-	draw_queued = true;
-
-func _draw() -> void:
 	if draw_queued:
-		print("Drawing chart with %d points." % points.size());
-		draw_polyline(points, Color.WHITE, 2);
+		draw_polyline(scaled_points, Color.WHITE, 4);
 		draw_queued = false;
+		if settings.shading:
+			var shading_points: PackedVector2Array = scaled_points.duplicate();
+			shading_points.append(Vector2(scaled_points[scaled_points.size() - 1].x, size.y));
+			shading_points.append(Vector2(scaled_points[0].x, size.y));
+			draw_colored_polygon(shading_points, settings.shading_color, [], null);
 
 func get_max_and_min(values: Array) -> Dictionary:
 	var max_value: float = - INF;
@@ -67,3 +67,6 @@ func get_max_and_min(values: Array) -> Dictionary:
 		"max": max_value,
 		"min": min_value,
 	};
+
+func _resized() -> void:
+	draw_queued = true;
--- chartr/scripts/chartr_settings.gd
+++ chartr/scripts/chartr_settings.gd
@@ -1,3 +1,5 @@
 class_name ChartrSettings extends RefCounted
 
 var zero_origin: bool = true;
+var shading: bool = true;
+var shading_color: Color = Color(0, 0, 0, 1);
